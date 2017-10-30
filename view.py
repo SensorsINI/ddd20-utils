1b39da0ed275fae39f3f9b857ac328247e80bbf0
Yuhuang Hu <duguyue100@gmail.com>
2017-10-30 16:55:54 +0100

    update an option of rotating

--- view.py
+++ view.py
@@ -513,10 +513,18 @@ def caer_event_from_row(row):
     return int(sys_ts) * 1e-6, unpack_data(d)
 
 
-
 if __name__ == '__main__':
 
     fname = sys.argv[1].strip()
+    try:
+        r180_opts = sys.argv[2].strip()
+        print (r180_opts)
+        if r180_opts == "true":
+            r180 = True
+        else:
+            r180 = False
+    except:
+        r180 = False
     # cv2.startWindowThread()
     c = Controller(fname,)
     m = MergedStream(HDF5Stream(fname, VIEW_DATA))
@@ -524,20 +532,20 @@ if __name__ == '__main__':
     t = time.time()
     t_pre = 0
     t_offset = 0
-    r180=False
+    #  r180=False
     print('recording duration', (m.tmax - m.tmin) * 1e-6, 's')
     # direct skip by command line
 #    if len(sys.argv)==1:
 #        print "usage: view.py file.hdf5\nview.py file.hdf5 tmin\nview.py file.hdf5 %n"
     
-    if len(sys.argv) == 3:
-        n_, type_ = sys.argv[2][:-1].strip(), sys.argv[2][-1].strip()
-        if type_ == '%':
-            m.search((m.tmax - m.tmin) * 1e-2 * float(n_) + m.tmin)
-#        elif type_=='r':
-#            r180=True
-        else:
-            m.search(float(n_) * 1e6 + m.tmin)
+#      if len(sys.argv) == 3:
+#          n_, type_ = sys.argv[2][:-1].strip(), sys.argv[2][-1].strip()
+#          if type_ == '%':
+#              m.search((m.tmax - m.tmin) * 1e-2 * float(n_) + m.tmin)
+#  #        elif type_=='r':
+#  #            r180=True
+#          else:
+#              m.search(float(n_) * 1e6 + m.tmin)
     v = Viewer(tmin=m.tmin * 1e-6, tmax=m.tmax * 1e-6,
             zoom=1.41, rotate180=r180,update_callback=c.update)
     # run main loop
