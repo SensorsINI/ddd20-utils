f86b18c62628ce64474461ea53129ad5a44e80ed
Yuhuang Hu <duguyue100@gmail.com>
2018-06-18 20:58:58 +0200

    update a data export script

---
+++ export-data-ral.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# night 1
+./export.py $HOME/spikeres/data/ddd17/jul09/rec1499656391.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499656391_export.hdf5
+# night 2
+./export.py $HOME/spikeres/data/ddd17/jul09/rec1499657850.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499657850_export.hdf5
+# night 3
+./export.py $HOME/spikeres/data/ddd17/aug01/rec1501649676.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501649676_export.hdf5
+# night 4
+./export.py $HOME/spikeres/data/ddd17/aug01/rec1501650719.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501650719_export.hdf5
+# night 5
+./export.py $HOME/spikeres/data/ddd17/aug05/rec1501994881.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501994881_export.hdf5
+# night 6
+./export.py $HOME/spikeres/data/ddd17/aug09/rec1502336427.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502336427_export.hdf5
+# night 7
+./export.py $HOME/spikeres/data/ddd17/aug09/rec1502337436.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502337436_export.hdf5
+# night 8
+./export.py $HOME/spikeres/data/ddd17/jul01/rec1498946027.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1498946027_export.hdf5
+# night 9
+./export.py $HOME/spikeres/data/ddd17/aug01/rec1501651162.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501651162_export.hdf5
+# night 10
+./export.py $HOME/spikeres/data/ddd17/jul02/rec1499025222.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499025222_export.hdf5
+# night 11
+./export.py $HOME/spikeres/data/ddd17/aug09/rec1502338023.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502338023_export.hdf5
+# night 12
+./export.py $HOME/spikeres/data/ddd17/aug09/rec1502338983.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502338983_export.hdf5
+# night 13
+./export.py $HOME/spikeres/data/ddd17/aug09/rec1502339743.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502339743_export.hdf5
+# night 14
+./export.py $HOME/spikeres/data/ddd17/jul01/rec1498949617.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1498949617_export.hdf5
+# night 15
+./export.py $HOME/spikeres/data/ddd17/aug12/rec1502599151.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502599151_export.hdf5
+
+# day 1
+./export.py $HOME/spikeres/data/ddd17/jul16/rec1500220388.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500220388_export.hdf5
+# day 2
+./export.py $HOME/spikeres/data/ddd17/jul18/rec1500383971.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500383971_export.hdf5
+# day 3
+./export.py $HOME/spikeres/data/ddd17/jul18/rec1500402142.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500402142_export.hdf5
+# day 4
+./export.py $HOME/spikeres/data/ddd17/jul28/rec1501288723.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501288723_export.hdf5
+# day 5
+./export.py $HOME/spikeres/data/ddd17/jul29/rec1501349894.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501349894_export.hdf5
+# day 6
+./export.py $HOME/spikeres/data/ddd17/aug01/rec1501614399.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501614399_export.hdf5
+# day 7
+./export.py $HOME/spikeres/data/ddd17/aug08/rec1502241196.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502241196_export.hdf5
+# day 8
+./export.py $HOME/spikeres/data/ddd17/aug15/rec1502825681.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1502825681_export.hdf5
+# day 9
+./export.py $HOME/spikeres/data/ddd17/jul02/rec1499023756.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499023756_export.hdf5
+# day 10
+./export.py $HOME/spikeres/data/ddd17/jul05/rec1499275182.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499275182_export.hdf5
+# day 11
+./export.py $HOME/spikeres/data/ddd17/jul08/rec1499533882.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1499533882_export.hdf5
+# day 12
+./export.py $HOME/spikeres/data/ddd17/jul16/rec1500215505.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500215505_export.hdf5
+# day 13
+./export.py $HOME/spikeres/data/ddd17/jul17/rec1500314184.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500314184_export.hdf5
+# day 14
+./export.py $HOME/spikeres/data/ddd17/jul17/rec1500329649.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1500329649_export.hdf5
+# day 15
+./export.py $HOME/spikeres/data/ddd17/aug05/rec1501953155.hdf5 --binsize 0.05 --out_file $HOME/data/exps/data/ddd17/rec1501953155_export.hdf5
